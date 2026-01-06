c595afeb3a6856d8a6982a6702303544ab1fc66d
Alen Ganopolsky <alenganopolsky@gmail.com>
2026-01-06 00:18:18 -0500

    added changes to main.py file and began working on code for s3 puts

--- backend/main.py
+++ backend/main.py
@@ -7,70 +7,74 @@
 # frontend ->
 # react, tailwind, vercel
 from fastapi import FastAPI
-import os
 from dotenv import load_dotenv
 import asyncio
 
-app = FastAPI()
+
 
 def lambda_handler(event, context):
-    import json
-    #from langchain.agents import create_agent
-    from fastapi import FastAPI
-    pass
+    return asyncio.run(search_method())
+    
 
 #@app.get("/search")
 async def search_method():
     from googleapiclient.discovery import build
     import os
     from dotenv import load_dotenv
+    import logging
+    import json
 
-   # outputs = []
-    urls = set()
+    try:
 
-    load_dotenv()
-    yt_api_key= os.getenv("YT_API_KEY")
+    # outputs = []
+        urls = set()
 
-    # should be replaced with search params
-    input_context: dict[str, tuple] = {"artists": ("Prettifun",)}
-    youtube = build("youtube", "v3", developerKey=yt_api_key)
+        load_dotenv()
+        yt_api_key= os.getenv("YT_API_KEY")
 
-
-    outputs = [search_video(f"royalty free {artist} loop kit", youtube) for artist in input_context["artists"]]
-    results = await asyncio.gather(*outputs)
-    print(results)
-
-    for video in results[0]:
-        url = f"https://youtube.com/watch?v={video['id']['videoId']}"
-        urls.add(url)
-
-    print(urls)
-    #return results
-    return urls
-
-async def search_video(query: str, yt, max_results=1, order="date", videoDuration="short"):
-
-    # include published after maybe
-    request = yt.search().list(
-        q=query,
-        part="id,snippet",
-        maxResults=max_results,
-        type="video",
-        order=order,
-        videoDuration=videoDuration
-
-    )
-
-    response = await asyncio.to_thread(request.execute)
-
-    return response["items"]
+        # should be replaced with search params that come from the lambda
+        input_context: dict[str, tuple] = {"artists": ("Prettifun",)}
+        youtube = build("youtube", "v3", developerKey=yt_api_key)
 
 
+        outputs = [search_video(f"royalty free {artist} loop kit", youtube) for artist in input_context["artists"]]
+        results = await asyncio.gather(*outputs)
+        logging.info(results)
 
-def extract_audio(video_id: str):
-    import yt_dlp
-    url = f"https://youtube.com/watch?v={video_id}"
+        for video in results[0]:
+            url = f"https://youtube.com/watch?v={video['id']['videoId']}"
+            urls.add(url)
 
+        logging.info(urls)
+
+        return urls
+    
+    except Exception as e:
+        print('here')
+        logging.error(e)
+
+async def search_video(query: str, yt, max_results=2, videoDuration="short") -> dict[str]:
+    import logging
+
+    try: 
+        # include published after maybe
+        request = yt.search().list(
+            q=query,
+            part="id,snippet",
+            maxResults=max_results,
+            type="video",
+            videoDuration=videoDuration
+
+        )
+
+        response = await asyncio.to_thread(request.execute)
+        logging.info(response)
+
+        return response["items"]
+    
+    except Exception as e:
+        print('here2')
+        logging.error(e)
 
 
 
