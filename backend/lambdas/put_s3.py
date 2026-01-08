aba815df780ac6956eaef4097458fd26863b5651
Alen Ganopolsky <alenganopolsky@gmail.com>
2026-01-08 02:14:14 -0500

    misc changes

---
+++ backend/lambdas/put_s3.py
@@ -0,0 +1,101 @@
+
+# a microservice that takes the downloaded, parsed yt audio files and places them in s3 -> which is then going to be used by the RAG model in a separate pipeline
+# all code for downloading yt videos, parsing them, and sending them to s3 is done here
+
+
+import os
+import logging
+
+
+def lambda_handler(event, context):
+    
+    """
+    Downloads a list of urls to the /tmp directory of the lambda function, subsequently pastes the mp3 files into s3. 
+    The idea is to then send this data to the RAG model
+    """
+    try:
+
+        # storage size of /tmp directory is 512mb -> can change this later
+        url_dir = "/tmp"
+        bucket_name = "rag-project-bucket-dev"
+
+        urls = event["urls"]
+        for url in urls:
+            download_file(url)
+        
+        filenames = os.listdir(url_dir)
+
+        for filename in filenames:
+            upload_file(filename, bucket_name)
+    
+    except Exception as e:
+        logging.error(e)
+
+
+def download_file(url: str, download_dir = "/tmp"):
+    from yt_dlp import YoutubeDL
+    import os
+
+    # yt_dlp config options here
+    format = os.path.join(download_dir, '%(title)s.%(ext)s')
+
+    ydl_opts = {
+
+        'format': 'bestaudio/best',
+        'extract_audio': True,          
+        'audio_format': 'mp3',           
+        'audio_quality': '192',
+        'outtmpl': f'{format}',
+        'noplaylist': True,
+
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
+    
+
+def upload_file(file_name, bucket, object_name=None):
+
+    import logging
+    import boto3
+    from botocore.exceptions import ClientError
+    import os
+
+
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # Process the content as needed
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+
+    except ClientError as e:
+        logging.error(e)
+        return False
+    
+    return True
+
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
---
+++ backend/rag_model/replicate_api.py
@@ -0,0 +1,5 @@
+import langchain
+import replicate_api
+
+def call_model():
+    pass
